93344ac0982818b2ab1dd022b3a95c16e4adc45c
Shuma Yoshioka <s64.stdio@gmail.com>
2018-04-27 20:40:07 +0900

    Initial commit

---
+++ src/gnome_favorites_edit/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse
+import gi
+from gi.repository import Gio, GLib
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('mode', choices = ['put', 'remove'])
+    parser.add_argument('app', help = 'Filename of application entry. e.g. `app.desktop`.')
+    parser.add_argument('--ignore-errors', action = 'store_true')
+
+    args = parser.parse_args()
+
+    schema = Gio.Settings('org.gnome.shell')
+    values = schema.get_value('favorite-apps').unpack()
+
+    already = args.app in values
+
+    if args.mode == 'put':
+        if already:
+            if not args.ignore_errors: raise RuntimeError('Already added.')
+        else:
+            schema.set_value('favorite-apps', GLib.Variant('as', values + [args.app]))
+    elif args.mode == 'remove':
+        if not already:
+            if not args.ignore_errors: raise RuntimeException('Not in favorites.')
+        else:
+            schema.set_value('favorite-apps', GLib.Variant('as', filter(lambda x: x != args.app, values)))
+
+if __name__ == '__main__': main()
---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'gnome-favorites-edit',
+    version = '0.0.1',
+    description = 'Edit dash favorites of gnome-shell.',
+    author = 'Shuma Yoshioka',
+    author_email = 's64.stdio@gmail.com',
+    packages = ['gnome_favorites_edit'],
+    entry_points = {
+        'console_scripts': [
+            'gnome-favorites-edit=gnome_favorites_edit.main:main',
+        ]
+    }
+)
