48cbcff868eeda54f508201492410db0a4a0b754
Shuma Yoshioka <s64.stdio@gmail.com>
2018-04-28 17:40:34 +0900

    Add metadata

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name = 'gnome-favorites-edit',
     version = '0.0.1',
     description = 'Edit dash favorites of gnome-shell.',
+    long_description = 'CLI app of dash favorites editor for gnome-shell.',
     author = 'Shuma Yoshioka',
     author_email = 's64.stdio@gmail.com',
     url = 'https://github.com/S64/gnome-favorites-edit',
+    license = 'Apache License 2.0',
     packages = ['gnome_favorites_edit'],
     entry_points = {
         'console_scripts': [
